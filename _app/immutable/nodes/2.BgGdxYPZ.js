f565a97ba12c90d9e279ebb7703dc2220857b309
hinjai2008 <hinjai2008@users.noreply.github.com>
2026-03-28 17:00:37 +0000

    deploy: 60d88d7c0b115d60a91538be4f29694d5afec8dd

---
+++ _app/immutable/nodes/2.BgGdxYPZ.js
@@ -0,0 +1,1 @@
+import"../chunks/CWj6FrbW.js";import"../chunks/C605Jdtw.js";import{t as o,a as p}from"../chunks/B9kNn5Xa.js";var e=o("<h1>Default page</h1>");function i(a){var t=e();p(a,t)}export{i as component};
