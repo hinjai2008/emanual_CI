203cb144f83433f8ffda688a09e29e0571610592
hinjai2008 <hinjai2008@users.noreply.github.com>
2026-03-29 06:30:41 +0000

    deploy: d8f2b2e70b5b3411f77dfaa5df3796412fa31470

--- _app/immutable/nodes/2.BkATw-Ul.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/CWj6FrbW.js";import"../chunks/B_q7TVae.js";import{t as o,a as p}from"../chunks/BKY_o757.js";var e=o("<h1>Default page</h1>");function i(a){var t=e();p(a,t)}export{i as component};
